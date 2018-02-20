4370dc33498fad9e35048df92fb9d434939a9de5
Bradley Wellman <bradley.m.wellman@gmail.com>
2018-02-20 09:48:37 -0500

    Added an enum for chat state and the shell of a function to handle blocking on read

--- hw1/code/include/client.h
+++ hw1/code/include/client.h
@@ -3,8 +3,20 @@
 
 #include <unistd.h>
 
+#define BUFSIZE 1024
+
 extern const char* cli_usage;
 extern char ** conn_info;
+
+typedef enum{
+    LOGIN,
+    LIST_USER,
+    MESSAGE_TO,
+    LOGOUT
+} ChatState_t;
+
+
+int handle_read(int sockfd, char * buf, ChatState_t state);
 char parse_args(int argc, char * const argv[], char * conn_info[]);
 int login(int sockfd, char *username, char *buf);
 #endif
